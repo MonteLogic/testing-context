90020a11e898216b682ebd9a68f795e646cfc634
Monte Logic <dchavours@gmail.com>
2023-01-22 13:42:03 -0600

    Multiple value change functions

--- src/components/GandChild.tsx
+++ src/components/GandChild.tsx
@@ -5,15 +5,30 @@ import { ThemeContext } from "../pages/index";
 export default function GrandChild(input: any) {
   console.log(input);
 
-  // const { darkTheme, setDarkTheme } = useContext(ThemeContext);
+  const { inputValue, setInputVal } = useContext(ThemeContext);
 
-  const handleClick = () => {
+  console.log(inputValue);
+
+  const makeVal2 = () => {
+    setInputVal("value2");
+  };
+  const makeVal3 = () => {
     console.log(input);
-    // setDarkTheme((prevTheme) => !prevTheme);
+    setInputVal("value3");
+  };
+  const makeVal4 = () => {
+    console.log(input);
+    setInputVal("value4");
   };
   return (
-    <button onClick={handleClick}>
-      Toggle Theme
-    </button>
+    <>
+      <button onClick={makeVal2}>Make value2</button>
+      <br />
+      <br />
+      <button onClick={makeVal3}>Make value3</button>
+      <br />
+      <br />
+      <button onClick={makeVal4}>Make value4</button>
+    </>
   );
 }
